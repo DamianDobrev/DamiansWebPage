93a4d9f67e617d4c8003c1e23703ad493f622135
Damian Dobrev <dmdbrv@gmail.com>
2015-03-12 04:23:42 +0200

    Skills are automatically added. Every skill is an object. Only information and responsive left.

--- app/controllers/SkillsCtrl.js
+++ app/controllers/SkillsCtrl.js
@@ -1,3 +1,45 @@
 app.controller('SkillsCtrl', ['$scope', function($scope){
+    $scope.skillsItems = [
+        {
+            name : 'C#',
+            knowledgePercent : 60,
+            description : 'I have basic knowledge of C# as a language and OOP principles. ' +
+                'I have done several projects with it including 1 game, 1 backend, 1 teamwork project in Softuni.'
+        },
+        {
+            name : 'Java',
+            knowledgePercent : 30,
+            description : 'I have basic knowledge of C# as a language and OOP principles. ' +
+                'I have done several projects with it including 1 game, 1 backend, 1 teamwork project in Softuni.'
+        },
+        {
+            name : 'JavaScript',
+            knowledgePercent : 10,
+            description : 'I have basic knowledge of C# as a language and OOP principles. ' +
+                'I have done several projects with it including 1 game, 1 backend, 1 teamwork project in Softuni.'
+        },
+        {
+            name : 'HTML',
+            knowledgePercent : 20,
+            description : 'I have basic knowledge of C# as a language and OOP principles. ' +
+                'I have done several projects with it including 1 game, 1 backend, 1 teamwork project in Softuni.'
+        },
+        {
+            name : 'CSS',
+            knowledgePercent : 90,
+            description : 'I have basic knowledge of C# as a language and OOP principles. ' +
+                'I have done several projects with it including 1 game, 1 backend, 1 teamwork project in Softuni.'
+        }
+    ];
+
+    $scope.showPopup = function (item) {
+        $scope.itemInfo = item;
+        $('.popup').show();
+    };
+
+    $scope.closePopup = function () {
+        $('.popup').hide();
+        $scope.itemInfo = null;
+    };
 
 }]);
